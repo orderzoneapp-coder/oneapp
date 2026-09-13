1abd80f452e21e084a1d7c57fc3f463025cc16e9
orderzoneapp-coder <orderzoneapp@gmail.com>
2026-09-13 14:06:20 +0900

    test(smartinput): add source preparation UI contract marker

---
+++ smartinput/source-preparation-ui-contract.js
@@ -0,0 +1,7 @@
+export const SOURCE_PREPARATION_UI_CONTRACT = Object.freeze({
+  version: '1.0.0',
+  mappingOwner: 'left-source-panel',
+  centralHeaderFilter: 'hover',
+  centralHeaderSearchIcon: false,
+  applyMode: 'explicit-batch'
+});
